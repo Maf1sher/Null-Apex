5aeba14bf6b7120b7f89556f987a99e7f3625c27
Maf1sher <kubas307@gmail.com>
2026-09-26 00:03:37 +0200

    fix: capture dragon render argument in mixin

--- src/main/java/dev/nullapex/mixin/EnderDragonRendererMixin.java
+++ src/main/java/dev/nullapex/mixin/EnderDragonRendererMixin.java
@@ -1,5 +1,6 @@
 package dev.nullapex.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import dev.nullapex.dragon.movement.DragonFlightVisualState;
 import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,7 +18,7 @@ abstract class EnderDragonRendererMixin {
         ),
         index = 0
     )
-    private float nullApex$useCustomFlightPitch(float vanillaPitch, EnderDragon dragon) {
+    private float nullApex$useCustomFlightPitch(float vanillaPitch, @Local(argsOnly = true) EnderDragon dragon) {
         Float customPitch = DragonFlightVisualState.flightPitchDegrees(dragon);
         return customPitch == null ? vanillaPitch : customPitch;
     }
